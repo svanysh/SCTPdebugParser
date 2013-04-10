6bc695950df34646393e73a3a8835a42d3c93006
Sergey Vanyshev <svanysh@mera.ru>
2013-04-10 08:32:31 +0400

    input string added

--- consoleTester/Program.cs
+++ consoleTester/Program.cs
@@ -41,11 +41,24 @@ namespace consoleTester
 			Console.WriteLine("sctphost parser");
 			
 			SctpEndpoint ep = new SctpEndpoint(1000, "192.168.10.1", "192.168.10.2");
-			
+			/*
 			for (ushort i=1; i<10;i++)
 			{
 				ep.AddAssocaitioin(i, "192.168.0." + i.ToString());
-			}
+			}*/
+			
+			String input = "0001: |---------------- SCTP ASSOCIATION  28 -----------------|\n"+
+"0001: |-------------------------------------------------------|"+
+"0001: | localPort:             3905"+
+"0001: | remotePort:            3905"+
+"0001: | dscp:                  0"+
+"0001: | ulpKey:                0x43e0f728"+
+"0001: | switchbackThreshold:   1"+
+"0001: | recentSuccessfulHBs:   0"+
+"0001: | sctpProfileId:         0"+
+"0001: |-----------------Path Information----------------------\n"+
+"0001: | abcd::83a0:4310 - abcd::83a0:4315 | preferred and active primary | last used | active   (+) | pmr: 0 ";
+
 			
 			foreach(SctpAssociation assoc in ep.Associations)
 			{
